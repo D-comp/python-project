ed65c648c37238dc8977234fe92e4deb0daef84e
gribanovskii-dmitrii <sselifanov6@gmail.com>
2024-10-31 12:25:46 +0300

    modified:   main.py 	modified:   setup.sh

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ output_path = '/tmp'
 
 # Функция для загрузки аудио с YouTube и конвертации в MP3
 def download_audio(youtube_url):
+    ffmpeg_path = '/usr/bin/ffmpeg'  # Укажите путь к ffmpeg
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': os.path.join(output_path, '%(title)s.%(ext)s'),
@@ -22,6 +23,7 @@ def download_audio(youtube_url):
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
+        'ffmpeg_location': ffmpeg_path,  # Укажите путь к ffmpeg
     }
 
     try:
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 apt-get update
 apt-get install -y ffmpeg
+ffmpeg -version  # Проверка установленной версии ffmpeg
+echo "Путь к ffmpeg: $FFMPEG_PATH"  # Выводим путь в логах
